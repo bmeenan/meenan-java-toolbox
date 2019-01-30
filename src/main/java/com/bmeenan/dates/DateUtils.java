4105aa13c468a67a698e59807fcebf2895f8bf15
GTDEV1-W10\bmeenan <bmeenan1@its.jnj.com>
2019-01-30 09:51:24 -0500

    java 7 style date maniuplation added along with associated unit tests

--- src/main/java/com/bmeenan/dates/DateUtils.java
+++ src/main/java/com/bmeenan/dates/DateUtils.java
@@ -1,6 +1,8 @@
 package com.bmeenan.dates;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Objects;
 
 public class DateUtils {
 
@@ -8,7 +10,15 @@ public class DateUtils {
 	 * Return the same date passed in with the day of the day set to 1
 	 */
 	public Date getReportStartDate(Date startDate) {
-		return null;
+
+		if (Objects.isNull(startDate)) {
+			return null;
+		}
+
+		Calendar cal = getCalendarInstance(startDate);
+		cal.set(Calendar.DAY_OF_MONTH, cal.getActualMinimum(Calendar.DAY_OF_MONTH));
+
+		return cal.getTime();
 	}
 
 	/**
@@ -17,7 +27,29 @@ public class DateUtils {
 	 */
 
 	public Date getReportEndDate(Date endDate) {
-		return null;
+
+		if (Objects.isNull(endDate)) {
+			return null;
+		}
+
+		Calendar cal = getCalendarInstance(endDate);
+		cal.set(Calendar.DAY_OF_MONTH, cal.getActualMaximum(Calendar.DAY_OF_MONTH));
+
+		return cal.getTime();
+	}
+
+	private Calendar getCalendarInstance(Date seedDate) {
+		// init with date passed in
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(seedDate);
+
+		// set hours, minutes, seconds, milliseconds to 0
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
+
+		return cal;
 	}
 
 }
--- src/test/java/com/bmeenan/dates/DateTests.java
+++ src/test/java/com/bmeenan/dates/DateTests.java
@@ -4,12 +4,10 @@ import java.util.Calendar;
 import java.util.Date;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-@Ignore
 @RunWith(MockitoJUnitRunner.class)
 public class DateTests {
 
@@ -18,25 +16,41 @@ public class DateTests {
 	@Test
 	public void whenStartDateIsMiddleOfTheMonth_ReportStartDateIsFirstOfTheMonth() {
 		Calendar submittedStartDate = Calendar.getInstance();
+
+		// set test start date to jan 15th - expected return should be Jan 1st
+		submittedStartDate.set(Calendar.MONTH, 0);
 		submittedStartDate.set(Calendar.DAY_OF_MONTH, 15);
 
 		Date reportStartDate = dateUtils.getReportStartDate(submittedStartDate.getTime());
 
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(reportStartDate);
-		Assert.assertEquals(15, cal.get(Calendar.DAY_OF_MONTH));
+		Assert.assertEquals(1, cal.get(Calendar.DAY_OF_MONTH));
 	}
 
 	@Test
 	public void whenEndDateIsMiddleOfTheMonth_ReportEndDateIsLastOfTheMonth() {
 		Calendar submittedEndDate = Calendar.getInstance();
+
+		// set test end date to jan 15th - expected return should be Jan 1st
+		submittedEndDate.set(Calendar.MONTH, 0);
 		submittedEndDate.set(Calendar.DAY_OF_MONTH, 15);
 
 		Date reportEndDate = dateUtils.getReportEndDate(submittedEndDate.getTime());
 
 		Calendar cal = Calendar.getInstance();
 		cal.setTime(reportEndDate);
-		Assert.assertEquals(cal.getActualMaximum(Calendar.DAY_OF_MONTH), cal.get(Calendar.DAY_OF_MONTH));
+		Assert.assertEquals(31, cal.get(Calendar.DAY_OF_MONTH));
+	}
+
+	@Test
+	public void whenStartDateIsNull_NullIsReturned() {
+		Assert.assertNull(dateUtils.getReportStartDate(null));
+	}
+
+	@Test
+	public void whenEndDateIsNull_NullIsReturned() {
+		Assert.assertNull(dateUtils.getReportEndDate(null));
 	}
 
 }

094754372956b57a6b5254bc19b6494cb8bff358
Meenan, Brian [JJCUS NON-J&J] <BMeenan1@its.jnj.com>
2019-02-04 20:44:19 -0500

    pushing solution to hackrank's java loop II challenge

---
+++ src/main/java/com/bmeenan/interview/ComplexLoop.java
@@ -0,0 +1,27 @@
+package com.bmeenan.interview;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-loops/problem
+ *
+ */
+public class ComplexLoop {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			int n = in.nextInt();
+			for (int j = 0; j < n; j++) {
+				a += b;
+				System.out.print(a + " ");
+				b *= 2;
+			}
+			System.out.println();
+		}
+		in.close();
+	}
+}

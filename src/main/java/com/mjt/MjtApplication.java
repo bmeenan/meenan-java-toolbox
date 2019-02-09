7853de97f5afad0bca6086de120c18487de2c56c
Meenan, Brian [JJCUS NON-J&J] <BMeenan1@its.jnj.com>
2019-02-09 10:32:06 -0500

    fixed application name

---
+++ src/main/java/com/mjt/MjtApplication.java
@@ -0,0 +1,11 @@
+package com.mjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjtApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MjtApplication.class, args);
+	}
+}
